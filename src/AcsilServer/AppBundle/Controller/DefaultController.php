8917db6a2d3add4c29fa453c7c48a79149410c25
iWooli <iwooli@iwooli.com>
2013-04-14 16:46:12 +0200

    Traduction

--- src/AcsilServer/AppBundle/Controller/DefaultController.php
+++ src/AcsilServer/AppBundle/Controller/DefaultController.php
@@ -6,18 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller {
 	public function indexAction() {
-		$em = $this -> getDoctrine() -> getManager();
-		$superAdmin = json_encode(array('ROLE_SUPER_ADMIN'));
 		
-		$isSuperAdmin = $em 
-			-> getRepository('AcsilServerAppBundle:User') 
-			-> findOneBy(array('roles' => $superAdmin));
-		
-		if ($isSuperAdmin) {
-			return $this -> redirect($this -> generateUrl('_acsil'));
-		} else {
-			return $this -> redirect($this -> generateUrl('_acsilAdmin'));
-		}
+		return $this -> redirect($this -> generateUrl('_acsil'));
 	}
 
 }
