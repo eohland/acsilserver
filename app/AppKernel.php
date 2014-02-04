49139ee34cb5554fa1fbac52740d3ab78d8685cf
Emmanuel <ohland_e@acsilserver.com>
2014-02-04 17:16:37 +0800

    Added (empty) APIBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
             new AcsilServer\AppBundle\AcsilServerAppBundle(),
+            new AcsilServer\APIBundle\APIBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/AcsilServer/APIBundle/APIBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace AcsilServer\APIBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class APIBundle extends Bundle
+{
+}
