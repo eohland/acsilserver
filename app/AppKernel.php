75d772c66db8476b2f3919e094e299dc0c95b7b0
Emmanuel <ohland_e@acsilserver.com>
2014-03-29 09:21:17 +0800

    Added FOSOAuthServerBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
+            new FOS\OAuthServerBundle\FOSOAuthServerBundle(),
             new FOS\RestBundle\FOSRestBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new AcsilServer\AppBundle\AcsilServerAppBundle(),
