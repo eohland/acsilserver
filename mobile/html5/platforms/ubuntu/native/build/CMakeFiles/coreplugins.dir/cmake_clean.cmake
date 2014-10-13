997b549eb08633eb264d432ad98f8eea5dc94309
galan_g <guillaume.galan@gmail.com>
2014-10-14 01:13:27 +0200

    HTML5 app -add logout -bugs fix

---
+++ mobile/html5/platforms/ubuntu/native/build/CMakeFiles/coreplugins.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+FILE(REMOVE_RECURSE
+  "cordova-ubuntu_automoc.cpp"
+  "cordovaubuntuplugin_automoc.cpp"
+  "coreplugins_automoc.cpp"
+  "CMakeFiles/coreplugins.dir/src/coreplugins.cpp.o"
+  "CMakeFiles/coreplugins.dir/src/plugins/org.apache.cordova.console/console.cpp.o"
+  "CMakeFiles/coreplugins.dir/src/plugins/org.apache.cordova.device/device.cpp.o"
+  "CMakeFiles/coreplugins.dir/coreplugins_automoc.cpp.o"
+  "libcoreplugins.pdb"
+  "libcoreplugins.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/coreplugins.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
