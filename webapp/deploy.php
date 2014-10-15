21ce928fb939ad6f27c04495677aed36c1569289
Emmanuel <ohland_e@epitech.net>
2014-10-15 10:14:48 +0200

    Update deploy.php path & execute sfcmd after pull

--- webapp/deploy.php
+++ webapp/deploy.php
@@ -6,10 +6,11 @@
 // Login   <ohland_e@epitech.net>
 // 
 // Started on  Fri Jan 31 23:20:29 2014 emmanuel ohland
-// Last update Fri Jan 31 23:21:01 2014 emmanuel ohland
+// Last update Wed Oct 15 10:11:46 2014 emmanuel ohland
 //
 
 header('text/plain');
 print shell_exec('git pull');
+print shell_exec('./sfcmd');
 
 ?>
