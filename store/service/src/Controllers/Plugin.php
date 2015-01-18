4ff83b99e20a0d4647fc156e8b1be1afbe00c77a
Emmanuel <ohland_e@epitech.net>
2015-01-18 13:59:23 +0100

    Add createTable & get functions to the Plugin ctrl

--- store/service/src/Controllers/Plugin.php
+++ store/service/src/Controllers/Plugin.php
@@ -1,13 +1,66 @@
 <?php
 namespace Controllers;
 
+use PDO;
+
 class Plugin extends \Utils\BaseController {
+  protected function createTable() {
+    try {
+      $sth = $this->pdo->prepare('
+        CREATE TABLE IF NOT EXISTS `plugins` (
+          `id`            INTEGER PRIMARY KEY AUTOINCREMENT,
+          `name`          TEXT NOT NULL,
+          `author`        INTEGER NOT NULL,
+          `description`   TEXT NOT NULL,
+          `version`       TEXT NOT NULL,
+          `create_date`   INTEGER,
+          `update_date`   INTEGER
+        );
+      ');
+      $sth->execute();
+    }
+    catch (Exception $e) {
+      error_log ('Plugin::createTable: ' . $e->getMessage());
+    }
+  }
+
   public function getAll() {
-    return 'Plugin List';
+    try {
+      $sth = $this->pdo->prepare('
+        SELECT
+          `id`, `name`, `author`,
+          `description`, `version`
+          `create_date`, `update_date`
+        FROM `plugins`;
+      ');
+      $sth->execute();
+      return $sth->fetchAll(PDO::FETCH_COLUMN);
+    }
+    catch (Exception $e) {
+      error_log ('Plugin::getAll: ' . $e->getMessage());
+    }
   }
 
   public function get($id) {
-    return 'Get plugin ' . $id;
+    try {
+      $sth = $this->pdo->prepare('
+        SELECT
+          `id`, `name`, `author`,
+          `description`, `version`
+          `create_date`, `update_date`
+        FROM `plugins`
+        WHERE id = :id;
+      ');
+      $sth->execute(array('id' => $id));
+      return $sth->fetchAll(PDO::FETCH_COLUMN);
+    }
+    catch (Exception $e) {
+      error_log ('Plugin::get: ' . $e->getMessage());
+    }
+  }
+
+  public function create($plugin) {
+    
   }
 }
 ?>
