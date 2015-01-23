168c244099436f5bdd98b278a596e84f5c252369
causse_g <guillaume.causse@epitech.eu>
2015-01-23 21:12:48 +0100

    Module: - module layout added. - module entity and form added. - "add module" works. - "launch module" works. - "delete module" works.

--- webapp/src/AcsilServer/AppBundle/Controller/ModuleController.php
+++ webapp/src/AcsilServer/AppBundle/Controller/ModuleController.php
@@ -8,12 +8,13 @@ use AcsilServer\AppBundle\Entity\Folder;
 use AcsilServer\AppBundle\Entity\ShareFile;
 use AcsilServer\AppBundle\Entity\RenameFile;
 use AcsilServer\AppBundle\Entity\MoveFile;
+use AcsilServer\AppBundle\Entity\Module;
 use AcsilServer\AppBundle\Form\ShareFileType;
 use AcsilServer\AppBundle\Form\RenameFileType;
 use AcsilServer\AppBundle\Form\DocumentType;
 use AcsilServer\AppBundle\Form\FolderType;
+use AcsilServer\AppBundle\Form\ModuleType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use FOS\OAuthServerBundle\Storage\OAuthStorage;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\Security\Acl\Domain\ObjectIdentity;
@@ -24,6 +25,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use \ZipArchive;
 use \RecursiveIteratorIterator;
+use FOS\OAuthServerBundle\Storage\OAuthStorage;
 
 //!  Modules operations Webapp class. 
 /*!
@@ -32,8 +34,10 @@ use \RecursiveIteratorIterator;
 class ModuleController extends Controller {
 
  
-  public function manageModuleAction() {
-    $clientManager = $this->get('fos_oauth_server.client_manager.default');
+	public function manageModuleAction($moduleId) {
+	
+	
+	$clientManager = $this->get('fos_oauth_server.client_manager.default');
     
     $storage = $this->get('fos_oauth_server.storage');
     
@@ -46,15 +50,96 @@ class ModuleController extends Controller {
     //print_r('</pre>');
     $user = $this -> getUser();
     
-    $token = $storage->createAccessToken($this->genAccessToken(), $client, $user, 1, 'foo bar');
-    return $this->render('AcsilServerAppBundle:Acsil:module.html.twig',
-			 array(
-			       'token' => $token->getToken(),
-			       ));
-    
+    $token = null; //$storage->createAccessToken($this->genAccessToken(), $client, $user, 1, 'foo bar');
+	
+	
+	
+	
+	
+	$em = $this -> getDoctrine() -> getManager();
+	$form = $this -> createForm(new ModuleType(), new Module());
+$listAllModules = $em 
+			-> getRepository('AcsilServerAppBundle:Module') 
+			-> findAll();
+			$currentCode = null;
+	if ($moduleId > 0)
+			{
+			$execModule = $em 
+			-> getRepository('AcsilServerAppBundle:Module') 
+			-> findOneBy(array('id' => $moduleId));
+			$currentCode = $execModule -> getCode();
+			$currentCode = str_replace("<q>","'", $currentCode);
+			$currentCode = htmlspecialchars_decode($currentCode);
+		
+			}
+		
+					return $this -> render('AcsilServerAppBundle:Acsil:module.html.twig',
+			array(
+				'form' => $form -> createView(),
+				'moduleId' => $moduleId,
+				'currentCode' => $currentCode,
+				'listAllModules' => $listAllModules,
+				//'token' => $token->getToken(),
+			));
 			
-  }
-  protected function genAccessToken() {
+	}
+
+
+	
+		/**
+	 * @Template()
+	 */
+	public function uploadModuleAction() {
+		$em = $this -> getDoctrine() -> getManager();
+    /**
+     * Create and fill a new document object
+    */
+		$module = new Module();
+		$request = $this -> getRequest();
+		$uploadedFile = $request -> files -> get('acsilserver_appbundle_moduletype');
+		$parameters = $request -> request -> get('acsilserver_appbundle_moduletype');
+		$file = $uploadedFile['file'];
+		$filename = $parameters['name'];
+		$code = file_get_contents($file->getPathName(), FILE_USE_INCLUDE_PATH);
+		 $code = htmlspecialchars($code);
+  $code = str_replace("'","<q>",$code);
+
+		$module -> setCode($code);
+		$module -> setName($filename);
+		if ($file == null) {
+		return $this -> redirect($this -> generateUrl('_module'));
+		}
+		if ($module -> getName() == null) {
+			$module -> setName($file -> getClientOriginalName());
+		}
+			
+		$em -> persist($module);
+		$em -> flush();
+		return $this -> redirect($this -> generateUrl('_module'));
+	}
+
+	
+	
+		
+		/**
+	 * @Template()
+	 */
+	public function deleteModuleAction($moduleId) {
+		$em = $this -> getDoctrine() -> getManager();
+if ($moduleId > 0)
+			{
+			$moduleToDelete = $em 
+			-> getRepository('AcsilServerAppBundle:Module') 
+			-> findOneBy(array('id' => $moduleId));
+
+			$em -> remove($moduleToDelete);
+		$em -> flush();
+			}
+	
+	return $this -> redirect($this -> generateUrl('_module'));
+	}
+	
+	  protected function genAccessToken() {
     if (@file_exists('/dev/urandom')) { // Get 100 bytes of random data
       $randomData = file_get_contents('/dev/urandom', false, null, 0, 100);
     } elseif (function_exists('openssl_random_pseudo_bytes')) { // Get 100 bytes of pseudo-random data
---
+++ webapp/src/AcsilServer/AppBundle/Form/ModuleType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AcsilServer\AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * This class aims to configure the form for a module
+ */
+class ModuleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name',  'text', array('required' => TRUE))
+            ->add('file', 'file', array('required' => TRUE))
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AcsilServer\AppBundle\Entity\Module'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'acsilserver_appbundle_moduletype';
+    }
+}
